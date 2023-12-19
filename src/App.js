531dff1ac62c5e8163eb481f6c8baf8e8d35a39a
ToktomushovElnur <Elnurbek2004mr@example.com>
2023-12-19 20:36:21 +0600

    Toktomushov Elnurbek 31-03

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css'
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const handleAddTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>Todo App</h1>
+        <div>
+          <input
+              type="text"
+              value={newTask}
+              onChange={(e) => setNewTask(e.target.value)}
+              placeholder="Введите новую задачу"
+          />
+          <button onClick={handleAddTask}>Добавить</button>
+        </div>
+        <ul>
+          {tasks.map((task, index) => (
+              <li key={index}>{task}</li>
+          ))}
+        </ul>
+      </div>
   );
 }
 
